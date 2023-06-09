2a5777b0ce56aa58e352a6d277604c7d15900ac2
alexanthonyso <soufflet.alexis@gmail.com>
2023-06-09 14:05:16 +0200

    adding and removing images works

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,5 @@
 const Book = require ('../models/book')
+const fs = require('fs');
 
 exports.postBook =(function(req, res) {
     console.log(req.body)
@@ -20,10 +21,9 @@ exports.postBook =(function(req, res) {
         res.status(200).json(books);
       })
       .catch(error => {
-
-        res.status(400).json({ error });
+      res.status(400).json({ error });
       });
-  };
+    };
 
   exports.getOneBook = (req,res,next)=>{
     console.log(req.params.id)
@@ -32,6 +32,63 @@ exports.postBook =(function(req, res) {
     .then(Book => res.status(200).json(Book))
     .catch(error => res.status(404).json({ error }));
 }
+
+
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+              console.log(book.ratings)
+                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+  };
+  
+  exports.deleteBook = (req, res, next) => {
+     Book.findOne({ _id: req.params.id})
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({message: 'Not authorized'});
+            } else {
+              console.log(book.userId)   
+                const filename = book.imageUrl.split('/images/')[1];
+                console.log(filename)  
+                fs.unlink(`images/${filename}`, () => {
+                    Book.deleteOne({_id: req.params.id})
+                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                        .catch(error => res.status(401).json(`Il y a une erreur ici` + {error}));
+                });
+            }
+        })
+        .catch( error => {
+          console.log(error); // Ajouter cette ligne pour afficher l'erreur sur la console
+          res.status(500).json({ error })})
+  };
+  
+  
+  exports.rateBook = (req,res,next)=>{
+    console.log('ok')
+      .then(() => res.status(200).json({ message: ' !'}))
+      .catch(error => res.status(400).json({ error }));
+  }
+  
+  exports.getBestBooks = (req,res,next)=>{
+    console.log('ok')
+      .then(() => res.status(200).json({ message: ' !'}))
+      .catch(error => res.status(400).json({ error }));
+    }
 
 
             
