652c94fd61feb77d26d1155d6540598d2b6aaaa5
alexanthonyso <soufflet.alexis@gmail.com>
2023-06-09 13:23:51 +0200

    User registration and login works

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const userCtrl = require("../controllers/user");
+const userCtrl = require('../controllers/user');
 
-router.post("/api/auth/signup", userCtrl.signup);
-router.post("/api/auth/login", userCtrl.login);
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
