c3d254370177054747e909d36bbd1de2971ceaaf
alexanthonyso <soufflet.alexis@gmail.com>
2023-06-15 23:10:29 +0200

    functional notation

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const userRoutes = require('./routes/user')
 const bookRoutes = require('./routes/book')
 const path = require('path');
+
 
 // Connexion MONGODB
 
@@ -13,22 +13,23 @@ mongoose.connect('mongodb+srv://User:Grimoire@cluster0.ee21h6j.mongodb.net/?retr
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  const app = express();
-
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+const app = express();
 
 
-  
-  app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  app.use('/api/auth', userRoutes)
-  app.use('/api/books', bookRoutes)
-  app.use('/images', express.static(path.join(__dirname, 'images')));
-
+app.use(express.json())
+     
+app.use('/api/auth', userRoutes)
+app.use('/api/books', bookRoutes)
+//app.use('api/books/:id', bookRoutes)
+//app.use('/api/books/bestrating', bookRoutes)
+//app.use('/api/books/:id/rating', bookRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 module.exports = app;
--- controllers/book.js
+++ controllers/book.js
@@ -1,97 +1,130 @@
 const Book = require ('../models/book')
+const sharp = require('sharp');
 const fs = require('fs');
 
-exports.postBook =(function(req, res) {
-    console.log(req.body)
-    console.log(req.file)
-    const bookObject = JSON.parse(req.body.book)
+exports.postBook = async (req, res) => {
+  try {
+    const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
-    const book = new Book({
-        ...bookObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    const imagePath = `${req.protocol}://${req.get('host')}/images/`;
+ 
+    await sharp(req.file.path)
+      .resize(404, 568)
+      .toFormat('webp')
+      .toFile(`images/${req.file.filename.split('.')[0]}.webp`);
+
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.error(err);
+      }
     });
-    book.save()
-      .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
 
-  exports.getAllBooks = (req, res, next) => {
-    Book.find()
-      .then(books => {
-        res.status(200).json(books);
-      })
-      .catch(error => {
-      res.status(400).json({ error });
-      });
-    };
+    const book = new Book({
+      ...bookObject,
+      imageUrl: `${imagePath}${req.file.filename.split('.')[0]}.webp`,
+    });
 
-  exports.getOneBook = (req,res,next)=>{
-    console.log(req.params.id)
-    Book.findOne({ _id: req.params.id })
+    await book.save();
 
-    .then(Book => res.status(200).json(Book))
-    .catch(error => res.status(404).json({ error }));
+    res.status(201).json({ message: 'Livre enregistré !' });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+
+exports.getAllBooks = async (req, res, next) => {
+  try {
+    const books = await Book.find()
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(400).json({ error });
+  };
+};
+
+exports.getOneBook = async (req,res)=>{
+  try {
+    const book = await  Book.findOne({ _id: req.params.id })
+    res.status(200).json(book)
+  } catch (error) { 
+    res.status(404).json({ error });
+  }
 }
 
-
-exports.modifyBook = (req, res, next) => {
+exports.modifyBook = async (req, res, next) => {
+  try {
     const bookObject = req.file ? {
-        ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      ...JSON.parse(req.body.book),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
     delete bookObject._userId;
-    Book.findOne({_id: req.params.id})
-        .then((book) => {
-            if (book.userId != req.auth.userId) {
-                res.status(401).json({ message : 'Not authorized'});
-            } else {
-              console.log(book.ratings)
-                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
-                .then(() => res.status(200).json({message : 'Objet modifié!'}))
-                .catch(error => res.status(401).json({ error }));
-            }
-        })
-        .catch((error) => {
-            res.status(400).json({ error });
-        });
-  };
-  
-  exports.deleteBook = (req, res, next) => {
-     Book.findOne({ _id: req.params.id})
-        .then((book) => {
-            if (book.userId != req.auth.userId) {
-                res.status(401).json({message: 'Not authorized'});
-            } else {
-              console.log(book.userId)   
-                const filename = book.imageUrl.split('/images/')[1];
-                console.log(filename)  
-                fs.unlink(`images/${filename}`, () => {
-                    Book.deleteOne({_id: req.params.id})
-                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
-                        .catch(error => res.status(401).json(`Il y a une erreur ici` + {error}));
-                });
-            }
-        })
-        .catch( error => {
-          console.log(error); // Ajouter cette ligne pour afficher l'erreur sur la console
-          res.status(500).json({ error })})
-  };
-  
-  
-  exports.rateBook = (req,res,next)=>{
-    console.log('ok')
-      .then(() => res.status(200).json({ message: ' !'}))
-      .catch(error => res.status(400).json({ error }));
-  }
-  
-  exports.getBestBooks = (req,res,next)=>{
-    console.log('ok')
-      .then(() => res.status(200).json({ message: ' !'}))
-      .catch(error => res.status(400).json({ error }));
+
+    const book = await Book.findOne({_id: req.params.id});
+    if (book.userId != req.auth.userId) {
+      res.status(401).json({ message : 'Not authorized'});
+    } else {
+      await Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id});
+      res.status(200).json({message : 'Objet modifié!'});
     }
+  } catch(error) {
+    res.status(400).json({ error });
+  }
+};
 
 
-            
+exports.deleteBook = async (req, res, next) => {
+  try {
+    const book = await Book.findOne({ _id: req.params.id });
+    if (book.userId != req.auth.userId) {
+      res.status(401).json({ message: 'Not authorized' });
+    } else {
+      const filename = book.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, async () => {
+        try {
+          await Book.deleteOne({ _id: req.params.id });
+          res.status(200).json({ message: 'Objet supprimé !' });
+        } catch (error) {
+          res.status(401).json(`Il y a une erreur ici` + { error });
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+};
 
-            
+
+exports.rateBook = async (req,res,next)=>{
+  try {
+    const book = await Book.findOne({_id: req.params.id})
+    const arrayRating = book.ratings
+    const newRatingObject = {
+      userId: req.body.userId,
+      grade: req.body.rating
+
+    };
+    
+    if (book.userId != req.auth.userId){
+     arrayRating.push(newRatingObject) 
+      const allRatings = arrayRating.map(rating => rating.grade);
+      const averageRating = allRatings.reduce((a, b) => a + b, 0) / allRatings.length;
+      book.averageRating = averageRating;
+      await book.save();
+    }
+    res.status(200).json(book)
+  } catch (error) {
+  res.status(401).json( { error });
+  }
+}
+
+/*exports.getBestBooks = async (req,res,next)=>{
+  try{
+    console.log('meilleurs livres')
+    const books = await Book.find()
+    res.status(200).json(bestBooks)
+  } catch (error) {
+  res.status(401).json({ error });
+  }
+}*/
     
