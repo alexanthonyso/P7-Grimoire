61b4ebad06073819f55715d000df5de8cbc1d6ee
alexanthonyso <soufflet.alexis@gmail.com>
2023-06-22 16:45:05 +0200

    Add dotenv

--- controllers/user.js
+++ controllers/user.js
@@ -1,51 +1,68 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
+
 
 exports.signup = async (req, res) => {
   try {
-     // FORMAT EMAIL REGEX
-     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-     if (!emailRegex.test(req.body.email)) {
-      console.log("Adresse non valide.")
-      res.status(400).json("Adresse non valide" )
-       } else {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    const user = new User({
-      email: req.body.email,
-      password: hash
-    });
+    // Vérification du format de l'adresse e-mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(req.body.email))  {
+      // Erreur si l'adresse e-mail n'est pas valide
+      res.status(400).json("Adresse non valide")
+    }else if (!req.body.password) {
+        // Erreur si aucun mot de passe n'est fourni
+        res.status(400).json("Mot de passe manquant")
+    } else {
 
-    await user.save();
+      // Génération du hachage en utilisant bcrypt 
+      const hash = await bcrypt.hash(req.body.password, 10)
 
-    res.status(201).json({ message: 'Utilisateur créé !' })}
+      // Création d'un nouvel utilisateur avec l'adresse e-mail et le mot de passe haché
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      })
+      await user.save()
+      res.status(201).json({ message: 'Utilisateur créé !' })
+    }
   } catch (error) {
-    res.status(400).json({ error });
+    res.status(500).json({ error })
   }
-};
+}
 
 exports.login = async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email });
+  try {
+    // Recherche de l'utilisateur 
+    const user = await User.findOne({ email: req.body.email })
 
-        if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        }
-
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-        if (!validPassword) {
-          return res.status(401).json({ error: 'Mot de passe incorrect !' });
-        } else {
-            const userId = user._id;
-            const token = jwt.sign({ userId }, 'SECRET_KEY');
-            console.log (token)
-            res.status(200).json({
-                userId: user._id,
-                token: token
-            })
-        }   
-    } catch (error) {
-        res.status(400).json({ error });
+    if (!user) {
+      return res.status(401).json({ error: 'Paire login/mot de passe incorrecte' })
     }
- };
+
+    // Vérification du mot de passe 
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+    if (!validPassword) {
+      // Erreur si mot de passe incorrect
+      return res.status(401).json({ error: 'Paire login/mot de passe incorrecte' })
+    } else {
+      const userId = user._id
+
+      // Génération du token
+      const token = jwt.sign(
+        { userId },
+        process.env.CLE_SECRETE,
+        { expiresIn: '4h' }
+      )
+
+      // Réponse avec l'identifiant de l'utilisateur et le token
+      res.status(200).json({
+        userId: user._id,
+        token: token,
+      })
+    }
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+}
