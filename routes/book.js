a9f5fdd9c1087577fb300c5a01f73028e47d1cdc
alexanthonyso <soufflet.alexis@gmail.com>
2023-06-26 17:04:23 +0200

    ADDING COMMENTS

--- routes/book.js
+++ routes/book.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../middleware/auth')
-const multer = require('../middleware/multer-config')
-const resizedImage = require('../middleware/sharp-config')
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+const resizedImage = require("../middleware/sharp-config");
 
+const bookCtrl = require("../controllers/book");
 
-const bookCtrl = require('../controllers/book')
+//Récupération de tous les livres de la BDD
+router.get("/", bookCtrl.getAllBooks);
 
-router.get('/', bookCtrl.getAllBooks)
-router.get('/bestrating', bookCtrl.getBestBooks )
-router.get('/:id', bookCtrl.getOneBook)
-router.post('/:id/rating', auth, bookCtrl.rateBook)
-router.post('/', auth, multer, resizedImage, bookCtrl.postBook)
-router.put('/:id', auth, multer,resizedImage, bookCtrl.modifyBook)
-router.delete('/:id', auth, bookCtrl.deleteBook)
+//Récupération des 3 livres les mieux notés
+router.get("/bestrating", bookCtrl.getBestBooks);
 
+//Récupération d’un livre en fonction de son id
+router.get("/:id", bookCtrl.getOneBook);
 
+//Ajout d’une note à un livre, et mise à jour de sa note moyenne
+router.post("/:id/rating", auth, bookCtrl.rateBook);
 
-module.exports = router
+//Ajout d’un nouveau livre à la BDD
+router.post("/", auth, multer, resizedImage, bookCtrl.postBook);
+
+//Modification d’un livre dans la BDD
+router.put("/:id", auth, multer, resizedImage, bookCtrl.modifyBook);
+
+//Suppression d’un livre dans la BDD
+router.delete("/:id", auth, bookCtrl.deleteBook);
+
+module.exports = router;
